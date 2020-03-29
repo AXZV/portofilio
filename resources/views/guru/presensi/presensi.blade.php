c400ca0894ffef9438c8876179f2645034423ed0
AXZV <unggulsurya97@gmail.com>
2020-03-29 11:07:39 +0700

    jobdesk 8,9

--- resources/views/guru/presensi/presensi.blade.php
+++ resources/views/guru/presensi/presensi.blade.php
@@ -50,33 +50,36 @@
                     @foreach($guru_kelas as $gk)
                         @foreach($pengajaran->where('kode_guru_kelas','=',$gk->kode) as $p)
                             <div class="col-xs-8 col-sm-6 col-md-4"><!-- item -->
-                                <!-- Card Narrower -->
-                                <div class="card card-cascade mb-4">
-                                <!-- Card image -->
-                                        <!-- Card content -->
-                                    <div class="card-body card-body-cascade">
-                                        <!-- Label -->
-                                        <h5 class="text-danger text-italic pb-2 pt-1 font-italic"><i class="fas fa-chalkboard-teacher"></i> #Sesi <?php echo $e++ ?></h5>
-                                        <!-- Title -->
-                                        <h4 class="font-weight-bold card-title">{{$gk->kelas->pelajaran->nama}}</h4>
-                                        <!-- Text -->
-                                        <div class="form-group">
-                                            <div class="input-group">
-                                                <div class="input-group-append">
-                                                    <span class="input-group-text fs-xl">
-                                                        <i class="fas fa-user-graduate"></i>
-                                                    </span>
-                                                </div>
-                                                <input disabled type="text" value="{{$p->siswa->count()}} Siswa" name="1" class="form-control">
+                             <!-- Card Narrower -->
+                                <div class="card card-cascade mb-4 hover-highlight">
+                                    <a href="presensi/log_presensi/{{$p->kode}}">
+                                        <div class="card-body card-body-cascade chover-highlight">
+                                            <h5 class="text-danger text-italic pb-2 pt-1 font-italic nama-kelas"><i class="fas fa-chalkboard-teacher"></i> #Sesi {{$p->guru_kelas->kelas->nama}}</h5>
+                                            <div class="text-center mb-4">
+                                                <span class="fas fa-chalkboard-teacher fa-4x text-danger icon-kelas"> </span>
                                             </div>
+                                            <div class="row no-gutters row-grid text-dark">
+                                                <div class="col-6">
+                                                    <div class="text-center py-3">
+                                                        <h5 class="mb-0 fw-700">
+                                                            Pelajaran
+                                                            <small class="text-muted mb-0">{{$p->guru_kelas->kelas->pelajaran->nama}}</small>
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6">
+                                                    <div class="text-center py-3">
+                                                        <h5 class="mb-0 fw-700">
+                                                            Siswa
+                                                            <small class="text-muted mb-0">{{$p->siswa->count()}} Orang</small>
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                            </div>                                
                                         </div>
-                                        <!-- Button -->
-                                        <div class="text-center">
-                                            <a href="presensi/log_presensi/{{$p->kode}}" class="btn btn-danger text-white">Presensi</a>
-                                        </div>
-                                    </div>
+                                    </a>
                                 </div>
-                                <!-- Card Narrower -->
+                            <!-- Card Narrower -->
 
                             </div><!-- item -->
                         @endforeach
